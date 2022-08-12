d23334dc77c36347a3556a80826223a8b0d7a6f7
Aline Avelino <aline.savelino@gmail.com>
2022-08-12 20:12:25 -0300

    feat: list individual pokemonTypes

--- js/script.js
+++ js/script.js
@@ -10,25 +10,30 @@ $.ajax({
         const pokeName = pokeInfo.name;
         const pokeImg =
           pokeInfo.sprites.other.dream_world.front_default;
-        const pokeTypes = pokeInfo.types;
+        let pokeTypes = pokeInfo.types;
+
+        async function getPokeTypes(pokeTypes) {
+          for (let i = 0; i < pokeTypes.length; i++) {
+            let ulPoke = await document.getElementById(pokeInfo.id);
+            let pokeType = pokeTypes[i].type.name;
+
+            $(ulPoke).append(`<li>${pokeType}</li>`);
+          }
+        }
 
         $("#pokemon-list").append(
           `
-            <li class='pokeGrassContainer'>
-                <h2>${pokeName}</h2>
-                <section>
-                    <ul class='type-list' id=${pokeInfo.id}>
-                    ${pokeTypes.map((pokeType) => {
-                      $(".type-list").append(
-                        `<li>${pokeType.type.name}</li>`
-                      );
-                    })}                        
-                    </ul>
-                    <img src=${pokeImg}>
-                </section>
+            <li class="pokeGrassContainer">
+              <h2>${pokeName}</h2>
+              <section>
+                <ul class="type-list" id=${pokeInfo.id}></ul>
+                <img src=${pokeImg}>
+              </section>
             </li>
           `
         );
+
+        $(document).ready(getPokeTypes(pokeTypes));
       });
     });
   },
